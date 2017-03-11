c424769ecd08f65a0275a2aba29945410a0091c6
Ivan <ivnsimakhin@gmail.com>
2017-03-11 23:06:38 +0300

    * rc/magit.el: added new bind for watch the diff

--- rc/magit.el
+++ rc/magit.el
@@ -1,4 +1,6 @@
 (use-package magit
   :bind (("C-c C-g" . magit-status)
          ("C-c q" . magit-commit-popup)
-         ("C-c l" . magit-commit-add-log)))
+         ("C-c l" . magit-commit-add-log)
+         ("C-c u" . magit-push-popup)
+         ("C-c d" . magit-diff-buffer-file)))
