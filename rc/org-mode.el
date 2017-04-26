a2ac9733639edf54c29f73256eb8445c0a2c05f2
Ivan <ivnsimakhin@gmail.com>
2017-04-26 20:30:31 +0300

    Added something to org mode config

--- rc/org-mode.el
+++ rc/org-mode.el
@@ -1,16 +1,25 @@
 (defun ivn/mark-done-and-archive ()
-  ("Mark TODO as DONE and archive it"
+  "Mark TODO as DONE and archive it"
   (interactive)
   (org-todo 'done)
-  (org-archive-subtree)))
+  (org-archive-subtree))
+(defun org-file-path(filename)
+  "Return abs path to file"
+  (concat (file-name-as-directory org-directory) filename))
 
 
 (use-package org
-  :bind (("C-c C-x C-s" . ivn/mark-done-and-archive))
+  :bind (
+         ("C-c C-x C-s" . ivn/mark-done-and-archive)
+         ("C-c a" . org-agenda)
+         )
   :config
   (setq org-log-done t)
   (setq org-src-fontify-natively t)
   (setq org-html-postamble nil)
   (setq org-directory "~/org")
-  (setq org-index-file "~/org/index.org")
-  (setq org-agenda-files (list org-index-file)))
+  (setq org-index-file (org-file-path "index.org"))
+  (setq org-archive-location
+        (concat (org-file-path "archive.org") "::* From %s"))
+  
+  )
