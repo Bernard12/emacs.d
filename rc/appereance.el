82dfa20675d887173d33f545560acbbebfb56f7c
ivan <abra@cadabra@>
2017-08-27 19:04:26 +0300

    Small code refactor

--- rc/appereance.el
+++ rc/appereance.el
@@ -8,38 +8,37 @@
 (setq-default indent-tabs-mode nil)
 (column-number-mode 1)
 
-(setq shell-file-name "zsh")
+;;; (setq shell-file-name "zsh")
 
 (use-package whitespace
   :config
   (global-whitespace-mode 1)
-  (setq whitespace-line-column 100))
+  (setq whitespace-line-column 120))
 
+;;; Show matching pairs of {,[,( and etc
 (use-package paren
   :config
   (show-paren-mode 1))
 
+;;; Smart indentation
 (use-package smart-tab
   :config
   (global-smart-tab-mode 1))
+
+
+(use-package bs
+  :bind (("C-c p" . bs-show)))
 
 (defun turnRainbow()
   (interactive)
   (rainbow-delimiters-mode 1)
   (rainbow-identifiers-mode 1))
 
-(use-package super-save)
-
-(use-package rainbow-delimiters
+(use-package super-save
   :config
-  (add-hook 'c++-mode-hook 'turnRainbow)
-  (add-hook 'c-mode-hook 'turnRainbow)
-  (add-hook 'emacs-lisp-mode-hook 'turnRainbow))
+  (super-save-mode 1))
 
-(use-package rainbow-identifiers
-  :config
-  (add-hook 'c++-mode-hook 'turnRainbow)
-  (add-hook 'c-mode-hook 'turnRainbow)
-  (add-hook 'python-mode-hook 'turnRainbow)
-  (add-hook 'emacs-lisp-mode-hook 'turnRainbow)
-  (add-hook 'go-mode-hook 'turnRainbow))
+
+;; This packages arent in use now
+(use-package rainbow-delimiters)
+(use-package rainbow-identifiers)
--- rc/rc.el
+++ rc/rc.el
@@ -1,37 +1,14 @@
 (require 'package)
 (require 'cask "~/.cask/cask.el")
+
 (cask-initialize)
 (package-initialize)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;;(load-file "~/.emacs.d/rc/toInstall.el")
 
 (setq user-full-name "Ivan")
 (setq user-mail-address "ivnsimakhin@gmail.com")
 
-(use-package bs
-  :bind (("C-c p" . bs-show)))
-
 (defun reloadInit()
   (interactive)
   (load-file "~/.emacs.d/init.el"))
-
-(defun tag()
-  (interactive)
-  (insert "- ")
-  (backward-char 2))
-
-(defun tmp()
-  (interactive)
-  (beginning-of-line)
-  (insert "(depends-on \"")
-  (end-of-line)
-  (insert "\")"))
-
-(defun commentReg(start end)
-  (interactive "r")
-  (save-excursion
-  (goto-char start)
-  (insert "/*\n")
-  (goto-char (+ end 2))
-  (insert "\n*/")))
