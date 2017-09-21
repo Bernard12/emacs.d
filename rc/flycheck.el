8e1dbf356152b68eab7b124867fff8d79b5afd02
ivn <ivnsimakhin@gmail.com>
2017-09-21 20:39:15 +0300

    Fixed flycheck-python setup

--- rc/flycheck.el
+++ rc/flycheck.el
@@ -10,11 +10,13 @@
   (flycheck-select-checker 'c/c++-gcc)
   (setq flycheck-gcc-language-standard "c++11"))
 
+(defun py-init()
+  (flycheck-mode 1)
+  (flycheck-select-checker 'python-flake8))
+
 (use-package flycheck
   :ensure t
   :config
   (add-hook 'c++-mode-hook 'c-init-fun)
   (add-hook 'c-mode-hook 'c-init-fun)
-  (add-hook 'python-mode-hook 'enable-flycheck)
-  (add-hook 'python-mode-hook (lambda ()
-                                (flycheck-select-checker 'python-flake8))))
+  (add-hook 'python-mode-hook 'py-init))
