bdad781adb9d83ade1aa7485cd24b3af8383f8fe
ivn <ivnsimakhin@gmail.com>
2018-01-26 17:35:18 +0300

    Small refactor

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
 (setq save-interprogram-paste-before-kill t)
 (setq-default c-basic-offset 4)
 (setq backup-directory-alist `((".*" . "~/.emacs.d/backups")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,7 +44,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (auctex ess-R-data-view ess clang-format company-lua flycheck-clang-tidy ein shackle helm-ag yasnippet web-mode use-package super-save smart-tab rainbow-identifiers rainbow-delimiters racer origami magit irony-eldoc helm gruvbox-theme gruber-darker-theme google-translate flycheck dashboard counsel company-web company-rtags company-restclient company-racer company-jedi company-irony-c-headers company-irony company-go cmake-project cmake-ide cmake-font-lock))))
+    (which-key auctex ess-R-data-view ess clang-format company-lua flycheck-clang-tidy ein shackle helm-ag yasnippet web-mode use-package super-save smart-tab rainbow-identifiers rainbow-delimiters racer origami magit irony-eldoc helm gruvbox-theme gruber-darker-theme google-translate flycheck dashboard counsel company-web company-rtags company-restclient company-racer company-jedi company-irony-c-headers company-irony company-go cmake-project cmake-ide cmake-font-lock))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
